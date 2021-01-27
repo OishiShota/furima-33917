022b2f3acf934cfb7922103a8421995122e1fc62
OishiShota <s92.oishi.shota@gmail.com>
2021-01-27 11:59:20 +0900

    ユーザーモデルバリデーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :name
+    validates :birthday
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' } do
     validates :first_name
     validates :last_name
