32d63b90f30ca6d19364ff43df52979f0cdd7974
OishiShota <s92.oishi.shota@gmail.com>
2021-01-27 17:06:10 +0900

    カテゴリーの選択（試運転でカテゴリーのみ）

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,15 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product_name
     validates :product_description
+    validates :price
+    validates :user
+    validates :image
+  end
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :state_id
-    validates :price
     validates :shipping_cost_id
     validates :prefecture_id
     validates :preparation_time_id
-    validates :user
-    validates :image
   end
 end
