f08afc38a0d3466f0a87f77cdb4666d4d8d92de8
OishiShota <s92.oishi.shota@gmail.com>
2021-01-27 16:40:12 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :product_name
+    validates :product_description
+    validates :category_id
+    validates :state_id
+    validates :price
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :preparation_time_id
+    validates :user
+    validates :image
+  end
 end
