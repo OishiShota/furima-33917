5e0e5cfc7b046a3e7df287952a32bf80cdd8cd63
OishiShota <s92.oishi.shota@gmail.com>
2021-01-28 08:04:25 +0900

    商品登録機能完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,25 @@
 class Item < ApplicationRecord
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
   belongs_to :shopping_cost
   belongs_to :prefecture
   belongs_to :preparation_time
-  has_one_attached :image
+
+
+  validates :product_name, presence:true, length:{maximum: 40}
+  validates :product_description, presence:true, length:{maximum: 1000}
+  validates :price, presence:true, numericality: {only_integer: true, greater_than: 299, less_than: 10000000}
 
   with_options presence: true do
-    validates :product_name
-    validates :product_description
-    validates :price
     validates :user
     validates :image
   end
+
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :state_id
