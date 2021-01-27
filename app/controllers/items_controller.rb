aab9ec763445114af5f954d79e8896fdbfc3b854
OishiShota <s92.oishi.shota@gmail.com>
2021-01-27 20:38:50 +0900

    商品情報の登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
   def new
+    @item = Item.new
   end
+
   def create
+    @item = Item.new(item_params)
+    binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :product_description, :category_id, :state_id, :price, :shipping_cost_id, :prefecture_id, :preparation_time_id).merge(user_id: current_user.id)
+  end
+
 end
