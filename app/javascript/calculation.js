a114b84c95b8a54c4116589b7f031c0065f7c9ca
OishiShota <s92.oishi.shota@gmail.com>
2021-01-28 10:10:20 +0900

    手数料、小数点以下切り上げ

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,10 +1,11 @@
 function calculation (){
   const itemPrice  = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
-    const addTaxPriceValue = itemPrice.value/10;
+    const price = itemPrice.value;
+    const addTaxPriceValue = Math.ceil(price/10);
     const addTaxPrice  = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${addTaxPriceValue}`;
-    const profitValue = (itemPrice.value/10*9);
+    const profitValue = (price - addTaxPriceValue);
     const profit  = document.getElementById("profit");
     profit.innerHTML = `${profitValue}`;
   });
