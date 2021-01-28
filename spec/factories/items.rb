650668d849740f9dcf09964152656119deb47e44
OishiShota <s92.oishi.shota@gmail.com>
2021-01-28 09:22:28 +0900

    item_factorybotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
+    product_name        {Faker::Lorem.sentence}
+    product_description {Faker::Lorem.sentence}
+    price               {Faker::Number.between(from: 300, to: 9999999)}
+    category_id         {Faker::Number.between(from: 2, to: 7)}
+    state_id            {Faker::Number.between(from: 2, to: 48)}
+    shipping_cost_id    {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id       {Faker::Number.between(from: 2, to: 48)}
+    preparation_time_id {Faker::Number.between(from: 2, to: 4)}
+    image               {Faker::Lorem.sentence}
     
+    association :user
   end
 end
