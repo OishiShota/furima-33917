1865009862f0869f9f5cc155f062e4b9c4893652
OishiShota <s92.oishi.shota@gmail.com>
2021-02-01 09:45:25 +0900

    単体テスト土台

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:adress)
+  end
+
+  describe '商品の出品' do
+    context "商品の出品ができる場合" do
+      it "正しく情報を記入すると、商品の出品ができる" do
+        expect(@adress).to be_valid
+      end
+    end
+    context "商品の出品ができない場合" do
+      it "商品画像がなければ登録できない" do
+      end
+    end
+  end
 end
