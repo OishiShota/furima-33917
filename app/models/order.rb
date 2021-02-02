44809e890c771141ab7606738a1bd5454a103971
OishiShota <s92.oishi.shota@gmail.com>
2021-02-02 13:05:13 +0900

    不要項目の削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
-  with_options presence: true do
-    validates :user
-    validates :item
-  end
-
 end
